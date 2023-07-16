259291e73ef1dd634eb6c922cb9a3d24575701b9
Alex Wulf <aw@dc.ag>
2023-07-16 20:02:27 +0200

    Get all new versions

---
+++ fetch.sh
@@ -0,0 +1,23 @@
+# Make the HTTP request and retrieve the JSON data
+response=$(curl -s "https://packagist.org/packages/phpunit/phpunit.json")
+
+# Extract the versions from the JSON data
+versions=$(echo "$response" | jq -r '.package.versions | keys[] | select(test("^((?!dev).)*$"))' | sort -V | tail -n 100)
+
+# Output the versions
+
+# Get the list of all tags
+tags=$(git tag)
+
+# Find the versions not included in tags
+not_in_tags=()
+for version in $versions; do
+    if [[ ! "$tags" =~ (^|[[:space:]])"$version"($|[[:space:]]) ]]; then
+        not_in_tags+=("$version")
+    fi
+done
+
+# Output the versions not included in tags
+for version in "${not_in_tags[@]}"; do
+    echo "$version"
+done
